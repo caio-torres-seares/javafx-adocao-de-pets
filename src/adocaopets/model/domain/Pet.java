7b7033110c73cdff87e68cccb8e0569d9a1a6bbf
Caio Torres Seares <caiot.seares@gmail.com>
2025-05-04 15:51:16 -0300

    add: adicionando novos atributos de idade e sexo à entidade Pet

--- src/adocaopets/model/domain/Pet.java
+++ src/adocaopets/model/domain/Pet.java
@@ -1,18 +1,36 @@
+// AUTOR: CAIO TORRES SEARES
+
 package adocaopets.model.domain;
 
-public class Pet {
+import java.io.Serializable;
+
+public class Pet implements Serializable{
     private int id;
     private String nome;
     private String especie;
     private String raca;
+    private int idade;
+    private SexoPetEnum sexo;
     private StatusPetEnum status; // "disponível", "indisponível" ou "adotado"
-    // adicionando comentário teste
+    
+    public Pet(){
+    }
 
-    public Pet(int id, String nome, String especie, String raca, StatusPetEnum status) {
-        this.id = id;
+    public Pet(String nome, String especie, String raca, int idade, SexoPetEnum sexo) {
         this.nome = nome;
         this.especie = especie;
         this.raca = raca;
+        this.idade = idade;
+        this.sexo = sexo;
+        this.status = StatusPetEnum.DISPONIVEL;
+    }
+    
+    public Pet(String nome, String especie, String raca, int idade, SexoPetEnum sexo, StatusPetEnum status) {
+        this.nome = nome;
+        this.especie = especie;
+        this.raca = raca;
+        this.idade = idade;
+        this.sexo = sexo;
         this.status = status;
     }
 
@@ -47,6 +65,22 @@ public class Pet {
     public void setRaca(String raca) {
         this.raca = raca;
     }
+    
+    public int getIdade() {
+        return idade;
+    }
+
+    public void setIdade(int idade) {
+        this.idade = idade;
+    }
+
+    public SexoPetEnum getSexo() {
+        return sexo;
+    }
+
+    public void setSexo(SexoPetEnum sexo) {
+        this.sexo = sexo;
+    }
 
     public StatusPetEnum getStatus() {
         return status;
@@ -55,4 +89,6 @@ public class Pet {
     public void setStatus(StatusPetEnum status) {
         this.status = status;
     }
+    
+    
 }
